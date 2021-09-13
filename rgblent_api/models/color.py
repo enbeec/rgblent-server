6a3ef96bee5e6cfd302c1fd76b3c3af1154e24f2
Val Currie <contact@va.lcuri.email>
2021-09-13 14:23:10 -0500

    docstrings for models

--- rgblent_api/models/color.py
+++ rgblent_api/models/color.py
@@ -3,6 +3,14 @@ from django.core.validators import MaxValueValidator
 
 
 class Color(models.Model):
+    """Color Model
+        Fields:
+            red (PositiveSmallIntegerField): red value
+            green (PositiveSmallIntegerField): green value
+            blue (PositiveSmallIntegerField): blue value
+            alpha (PositiveSmallIntegerField): alpha value
+            is_default (BooleanField): indicates if the color is one of the defaults
+        """
     red = models.PositiveSmallIntegerField(
         default=0, validators=[MaxValueValidator(255)])
     green = models.PositiveSmallIntegerField(
--- rgblent_api/models/user_color.py
+++ rgblent_api/models/user_color.py
@@ -6,6 +6,11 @@ USERCOLOR_LABEL_MAX_LENGTH = 100
 
 
 class UserColor(models.Model):
+    """ UserColor Model
+        user (ForeignKey): the user that favorited this color
+        color (ForeignKey): the favorited color information
+        label (CharField): the favoriting user's name for this color
+        """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     color = models.ForeignKey("Color", on_delete=models.RESTRICT)
     label = models.CharField(max_length=USERCOLOR_LABEL_MAX_LENGTH)
