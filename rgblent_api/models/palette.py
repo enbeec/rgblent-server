a28405b6b05483263c8b41e16549f7bd7f694a19
Val Currie <contact@va.lcuri.email>
2021-09-13 15:00:24 -0500

    fixed max_length

--- rgblent_api/models/palette.py
+++ rgblent_api/models/palette.py
@@ -7,4 +7,4 @@ PALETTE_NAME_MAX_LENGTH = 100
 class Palette(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
-    name = models.CharField(PALETTE_NAME_MAX_LENGTH)
+    name = models.CharField(max_length=PALETTE_NAME_MAX_LENGTH)
