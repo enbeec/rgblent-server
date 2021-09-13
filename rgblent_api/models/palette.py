7ddcc518d7c5c35145e6996d202eb9f979b3d23a
Val Currie <contact@va.lcuri.email>
2021-09-13 15:08:06 -0500

    docstrings

--- rgblent_api/models/palette.py
+++ rgblent_api/models/palette.py
@@ -5,6 +5,12 @@ PALETTE_NAME_MAX_LENGTH = 100
 
 
 class Palette(models.Model):
+    """ Palette Model
+
+        Fields:
+            user (ForeignKey): the user that created the palette
+            name (CharField): the name of the palette
+        """
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
     name = models.CharField(max_length=PALETTE_NAME_MAX_LENGTH)
--- rgblent_api/models/palette_color.py
+++ rgblent_api/models/palette_color.py
@@ -5,6 +5,13 @@ PALETTECOLOR_LABEL_MAX_LENGTH = 100
 
 
 class PaletteColor(models.Model):
+    """ Palette Color Model
+
+        Fields:
+            palette (ForeignKey): the palette this color belongs to
+            color (ForeignKey): the color to associate with the palette
+            label (CharField): the label for this color in the context of this palette
+        """
     palette = models.ForeignKey("Palette", on_delete=models.CASCADE)
     # RESTRICT keeps colors from being deleted if they are referenced somewhere
     color = models.ForeignKey("Color", on_delete=models.RESTRICT)
