2fa0d0fa2e550a7ed8f0a1515d8a57dcbff531d3
Val Currie <contact@va.lcuri.email>
2021-09-13 14:35:17 -0500

    changed get_user_model call to settings.AUTH_USER_MODEL

--- rgblent_api/models/user_color.py
+++ rgblent_api/models/user_color.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user
+from django.conf import settings
 
-User = get_user_model()
 USERCOLOR_LABEL_MAX_LENGTH = 100
 
 
@@ -11,6 +11,7 @@ class UserColor(models.Model):
         color (ForeignKey): the favorited color information
         label (CharField): the favoriting user's name for this color
         """
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     color = models.ForeignKey("Color", on_delete=models.RESTRICT)
     label = models.CharField(max_length=USERCOLOR_LABEL_MAX_LENGTH)
