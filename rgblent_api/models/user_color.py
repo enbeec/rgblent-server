e2ea41a376c38e34cda7133700ed8ebf1c8387f9
Val Currie <contact@va.lcuri.email>
2021-09-21 14:26:26 -0500

    added related names to models

--- rgblent_api/models/user_color.py
+++ rgblent_api/models/user_color.py
@@ -13,7 +13,8 @@ class UserColor(models.Model):
             label (CharField): the favoriting user's name for this color
         """
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
+                             related_name="colors", on_delete=models.CASCADE)
     # RESTRICT keeps colors from being deleted if they are referenced somewhere
-    color = models.ForeignKey("Color", on_delete=models.RESTRICT)
+    color = models.ForeignKey(
+        "Color", on_delete=models.RESTRICT)
     label = models.CharField(max_length=USERCOLOR_LABEL_MAX_LENGTH)
