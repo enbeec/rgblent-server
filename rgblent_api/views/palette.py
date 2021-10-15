6ed95605aa4b453104573e64e219ae3928bb22e3
Val Currie <contact@va.lcuri.email>
2021-10-15 00:33:32 -0500

    added example of default colors

--- rgblent_api/views/palette.py
+++ rgblent_api/views/palette.py
@@ -36,15 +36,15 @@ class PaletteSerializer(serializers.ModelSerializer):
         fields = ('name', 'colors')
 
     def get_isMine(self, obj):
-    """ Returns true if request user matches the palette being serialized
+        """ Returns true if request user matches the palette being serialized
 
-        Arguments:
-            self -- the serializer instance
-            obj -- the Palette being serialized
-        """
-     user = None
-      request = self.context.get("request")
-       if request and hasattr(request, "user"):
+            Arguments:
+                self -- the serializer instance
+                obj -- the Palette being serialized
+            """
+        user = None
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
             user = request.user
 
         if user is not None:
@@ -63,9 +63,72 @@ def default_palette(request):
 
         @apiSuccess (200) {Object} palette Retrieved default palette
         @apiSuccessExample {json} Success
-            {
-                TODO
-            }
+            [
+                  {
+                       "rgb_hex" => "#FFDF80",
+                           "red" => 255,
+                         "green" => 223,
+                          "blue" => 128,
+                       "builtin" => false,
+                    "is_default" => true
+                  },
+                  {
+                       "rgb_hex" => "#BFFF80",
+                           "red" => 191,
+                         "green" => 255,
+                          "blue" => 128,
+                       "builtin" => false,
+                    "is_default" => true
+                  },
+                  {
+                       "rgb_hex" => "#80FF9F",
+                           "red" => 128,
+                         "green" => 255,
+                          "blue" => 159,
+                       "builtin" => false,
+                    "is_default" => true
+                  },
+                  {
+                       "rgb_hex" => "#80FFFF",
+                           "red" => 128,
+                         "green" => 255,
+                          "blue" => 255,
+                       "builtin" => false,
+                    "is_default" => true
+                  },
+                  {
+                       "rgb_hex" => "#809FFF",
+                           "red" => 128,
+                         "green" => 159,
+                          "blue" => 255,
+                       "builtin" => false,
+                    "is_default" => true
+                  },
+                  {
+                       "rgb_hex" => "#BF80FF",
+                           "red" => 191,
+                         "green" => 128,
+                          "blue" => 255,
+                       "builtin" => false,
+                    "is_default" => true
+                  },
+                  {
+                       "rgb_hex" => "#FF80DF",
+                           "red" => 255,
+                         "green" => 128,
+                          "blue" => 223,
+                       "builtin" => false,
+                    "is_default" => true
+                  },
+                  {
+                       "rgb_hex" => "#FF8080",
+                           "red" => 255,
+                         "green" => 128,
+                          "blue" => 128,
+                       "builtin" => false,
+                    "is_default" => true
+                  }
+            ]
         """
     palette = Palette.objects.get(builtin=True, name="default")
     serializer = PaletteSerializer(palette, context={'request': request})
